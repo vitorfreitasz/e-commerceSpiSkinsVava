84e1fde70e577d03c4a55f1d9e69d9ef68b1d9fb
Vitor De Ávila Freitas <108691011+vitorfreitasz@users.noreply.github.com>
2022-11-22 20:42:47 -0300

    JavaScript Funcionando, JSON, e melhorias estéticas.
    Pesquisei e consegui implementar o JSON com o catalogo dos produtos, e junto ao JS botei eles na página. O Samuel colocou melhorias estéticas na página de contato.

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
-console.log(
-    "helloworld"
-    );
-
-function lucao(){
-    console.log("oi");
+let http = new XMLHttpRequest();
+http.open('get','produtos.json', true);
+http.send();
+http.onload = function(){
+    if(this.readyState == 4 && this.status==200){
+        let produtos = JSON.parse(this.responseText);
+        console.log(produtos)
+        let output = "";
+        for(let item of produtos){
+            output += `
+            <div class="produtos">
+                <img src="${item.imgskin}" alt="${item.imgskin}">
+                <p class="skin">${item.skin}</p>
+                <p class="preco">
+                    <span>R$</span>
+                    <span>${item.preco}</span>
+                </p>
+            </div>
+            `;
+        }
+        document.querySelector(".produtos").innerHTML = output;
+    }
 }
+
+
+/*{"Rifles":
+    [
+        {
+            "arma": "Vandal",
+            "skinsDaArma": ["Saqueadora","GlitchPop","Ion"],
+            "preços":[1750,1750,2175]
+        },
+        {
+            "arma": "Phantom",
+            "skinsDaArma": ["Blastx","GlitchPop","Ion"], 
+            "preços":[2175,1750,2175]
+        }
+    ]
+}*/
