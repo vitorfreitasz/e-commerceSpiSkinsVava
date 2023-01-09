871acc03210ca2ceb49b4296b7cb57758fe4e7ae
Vitor De Ávila Freitas <108691011+vitorfreitasz@users.noreply.github.com>
2023-01-09 11:48:27 -0300

    hgfhfg

--- js/carrinho.js
+++ js/carrinho.js
@@ -4,27 +4,3 @@ if(localStorage.getItem('token')==null){
     alert('Você precisa logar em sua conta para acessar o carrinho...')
     window.location.href='login.html'
 }
-
-let http = new XMLHttpRequest();
-http.open('get','produtos.json', true);
-http.send();
-http.onload = function(){
-    if(this.readyState == 4 && this.status==200){
-        let produtos = JSON.parse(this.responseText);
-        console.log(produtos)
-        let output = "";
-        for(let item of produtos){
-            output += `
-            <div class="produtos">
-                <img src="${item.imgskin}" alt="${item.imgskin}">
-                <p class="skin">${item.skin}</p>
-                <p id="preco">
-                    <span>VP&dollar; ${item.preco}</span>                  
-                </p>
-                <button id="botaocompra" onlick="addcart(${item.skin})">Comprar</button>
-            </div>
-            `;
-        }
-        document.querySelector("#produtosdisplay").innerHTML = output;
-    }
-}
