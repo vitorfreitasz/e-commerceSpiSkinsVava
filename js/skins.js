1622ddd7f7c2af8d05146a0a5bcfd69488bffa3b
Vitor De Ávila Freitas <108691011+vitorfreitasz@users.noreply.github.com>
2023-01-16 20:40:59 -0300

    a

---
+++ js/skins.js
@@ -0,0 +1,81 @@
+/* Aqui puxamos a lista dos produtos do JSON */
+
+let http = new XMLHttpRequest();
+http.open('get','produtos.json', true);
+http.send();
+http.onload = function(){
+    if(this.readyState == 4 && this.status==200){
+        let produtos = JSON.parse(this.responseText);
+        let output = "";
+        let outknifes= "";
+        for(let item of produtos){
+            if(item.classe == "Rifle"){
+                output += `
+                <div class="produtos">
+                <a href = "${item.linkskin}">
+                <img src="${item.imgskin}" alt="${item.imgskin}">
+                <p class="skin">${item.skin}</p>
+                <p id="preco">
+                <span>R&dollar; ${item.preco}</span>                  
+                </p></a>
+                <button id="botaocompra" onclick="addcart('${item.skin}')">Comprar</button>
+                </div>
+                `;
+            } if(item.classe == "Faca"){
+                outknifes += `
+                <div class="produtos">
+                <a href = "${item.linkskin}">
+                <img src="${item.imgskin}" alt="${item.imgskin}">
+                <p class="skin">${item.skin}</p>
+                <p id="preco">
+                <span>R&dollar; ${item.preco}</span>                  
+                </p></a>
+                <button id="botaocompra" onclick="addcart('${item.skin}')">Comprar</button>
+                </div>
+                `;
+            }
+        }
+        document.querySelector("#produtosrifles").innerHTML = output;
+        document.querySelector("#produtosfacas").innerHTML = outknifes;
+    }
+}
+
+dadosusuario = JSON.parse(localStorage.getItem('userLog'))
+let carrinho= dadosusuario.cart
+function addcart(x){
+    
+    carrinho.push(x)
+    console.log(carrinho)
+    usuario=JSON.parse(localStorage.getItem('userLog'))
+    //.filter(item => item.cart !== carrinho)
+    usuario.cart = carrinho
+    console.log(usuario)
+    localStorage.removeItem('userLog')
+    localStorage.setItem('userLog', JSON.stringify(usuario))
+    setTimeout(()=>{
+        window.location.href='carrinho.html'
+    }, 1000)
+}
+
+function salvadiv(x){
+    let prodt = x
+    console.log(prodt)
+    
+}
+
+
+function pagFinal(){
+    setTimeout(()=>{
+        window.location.href='finalizado.html'
+    }, 1000)
+}
+
+function usuario(){
+    if(localStorage.getItem('token')==null){
+        window.location.href='login.html'
+    } else{
+        window.location.href='perfil.html'
+    }
+}
+
+/* -------------------------------------------------------------------------------------- */
